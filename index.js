c44a85f0c2a2327bac91f2f94ffb2e2d2087e5a6
escowin <edwin@escowinart.com>
2022-04-21 08:39:36 -0500

    index.js:10-25 team building set up

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+// EMPLOYEE CONSTS
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Manager = require("./lib/Manager");
 const Intern = require("./lib/Intern");
+
+// TEAM BUILDING
+const teamBuilding = () => {
+    const team = () => {
+        return inquirer.prompt([
+            {
+            type: 'list',
+            name: 'employee',
+            message: 'team building starts now',
+            choices: ['manager', 'engineer', 'intern', 'n/a']
+            }
+        ]).then(employee => {
+            switch
+            // cases, cases, cases
+        })
+    }
+} 
