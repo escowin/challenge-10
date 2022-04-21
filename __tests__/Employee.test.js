fe7b04dbb023794842d9378a87dcb061c6a9b3f7
escowin <edwin@escowinart.com>
2022-04-21 07:37:43 -0500

    employee.test tdd pass (email test only passes if id number is included js:26)

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,27 @@ test('get employee stats as an object', () => {
     expect(employee.email).toEqual("email@email.com");
     // expect(employee.role).toEqual("Employee");
 });
+
+test('get employee name from getName()', () => {
+    const name = "dave";
+    const employee = new Employee(name);
+    expect(employee.getName()).toEqual(name);
+});
+
+test('get employee id from getId()', () => {
+    const id = 11;
+    const employee = new Employee('sean', id);
+    expect(employee.getId()).toEqual(id);
+});
+
+test('get employee email from getEmail()', () => {
+    const email = "email@email.com";
+    const employee = new Employee("lex", 11, email);
+    expect(employee.getEmail()).toEqual(email);
+});
+
+test('get employee role from getRole()', () => {
+    const role = 'Employee';
+    const employee = new Employee('smithers', role);
+    expect(employee.getRole()).toEqual(role);
+});
