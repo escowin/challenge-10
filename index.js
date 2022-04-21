659dc1703f529b744eaa11d88643199dbb0205ab
escowin <edwin@escowinart.com>
2022-04-21 10:50:02 -0500

    index.js:18-96 team & manager prompt & inquirer install

--- index.js
+++ index.js
@@ -15,11 +15,81 @@ const teamBuilding = () => {
             type: 'list',
             name: 'employee',
             message: 'team building starts now',
-            choices: ['manager', 'engineer', 'intern', 'n/a']
+            choices: ['manager', 'engineer', 'intern', 'no employees']
             }
-        ]).then(employee => {
-            switch
-            // cases, cases, cases
-        })
+        ]).then(employees => {
+            switch(employees.employee) {
+                case 'manager':
+                    managerInput();
+                    break;
+                case 'engineer':
+                    engineerInput();
+                    break;
+                case 'intern':
+                    internInput();
+                    break;
+            }
+        });
+    };
+    
+    const managerInput = () => {
+        console.log("assemble your team:")
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'managerName',
+                message: 'enter manager name',
+                validate: answer => {
+                    if (!answer) {
+                        return "we live in a society. who is the team manager?";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: 'enter manager id no.',
+                validate: answer => {
+                    // constrains input to accept only numbers
+                    const input = answer.match(/^[0-9]\d*$/);
+                    if (!answer) {
+                        return "numbers only. what is the manager's id no.?";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'enter manager email',
+                validate: answer => {
+                    if (!answer) {
+                        return "what is the manager's email?";
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'officeNumber',
+                message: 'enter manager office number',
+                validate: answer => {
+                    const input = answer.match(/^[0-9]\d*$/);
+                    if (!answer) {
+                        return "numbers only. what is the office number of this manager?";
+                    } else {
+                        return true;
+                    }
+                }
+            }
+        ])
+        
     }
 } 
+
+// CALLS
+teamBuilding();
