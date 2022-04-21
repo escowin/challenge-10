abfc60633f0a04b0da0e69f17b2638cca9eb1bdd
escowin <edwin@escowinart.com>
2022-04-21 07:24:49 -0500

    employee.test tdd pass (if js:9 commented out)

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,20 +1,10 @@
 const Employee = require('../lib/Employee');
-const Engineer = require('../lib/Engineer');
-
-jest.mock('../lib/Engineer');
-console.log(new Engineer());
 
 test('get employee stats as an object', () => {
-    const employee = new Employee('dave');
+    const employee = new Employee("dave", 11, "email@email.com");
 
-    expect(player.getStats()).toHaveProperty('engineer');
-    expect(player.getStats()).toHaveProperty('intern');
-    expect(player.getStats()).toHaveProperty('manager')
-
-    // name property
-    // expect(employee.name).toBe('dave');
-    // number property
-    // expect(employee.id).toEqual(expect.any(Number));
-    // name property
-    // expect(employee.email).toBe('employee@work.com');
+    expect(employee.name).toEqual("dave");
+    expect(employee.id).toEqual(11);
+    expect(employee.email).toEqual("email@email.com");
+    // expect(employee.role).toEqual("Employee");
 });
