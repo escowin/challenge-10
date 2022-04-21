511cc8871c35f0264fbd27ddedb049022e5d0679
escowin <edwin@escowinart.com>
2022-04-21 07:08:42 -0500

    employee.js set up

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,20 @@
 const Employee = require('../lib/Employee');
+const Engineer = require('../lib/Engineer');
 
-test('creates an employee object', () => {
+jest.mock('../lib/Engineer');
+console.log(new Engineer());
+
+test('get employee stats as an object', () => {
     const employee = new Employee('dave');
 
-    expect(employee.name).toBe('dave');
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(player.getStats()).toHaveProperty('engineer');
+    expect(player.getStats()).toHaveProperty('intern');
+    expect(player.getStats()).toHaveProperty('manager')
+
+    // name property
+    // expect(employee.name).toBe('dave');
+    // number property
+    // expect(employee.id).toEqual(expect.any(Number));
+    // name property
     // expect(employee.email).toBe('employee@work.com');
 });
