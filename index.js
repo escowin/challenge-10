3369e53b46010077f5d65578562b3f029fa80bda
escowin <edwin@escowinart.com>
2022-10-05 22:04:24 -0500

    pseudocode app logic

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-a = 'erased old app, starting fresh'
+// start app
+// - ask for team manager name, id#, email address, office number
+// - after user answers prompts, show user a menu (options: add engineer, add intern, finish building team)
 
-console.log(a)
+// select | engineer
+// - enter engineer name, id#, email, github username
+// - after user answers prompts, return to menu
+
+// select | intern
+// - enter intern name, id#, email, school
+// - after user answers prompts, return to menu
+
+// select | finish building team
+// - exit app, generate html
+
+// fs writeFile
+// - generate html page from that data
+
+// dist/profile.html
+// - clicking on email link will open default email program & populate the TO field with employee's email
+// - clicking on github link opens new tab
