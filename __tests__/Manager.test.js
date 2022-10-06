38e61a895cacc9e386d535d50848933d33b2abcb
escowin <edwin@escowinart.com>
2022-10-06 10:41:35 -0500

    paused at Manager tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -4,6 +4,12 @@ const Employee = require('../lib/Employee');
 // mock data
 jest.mock('../lib/Employee');
 
-test('create a manager object' () => {
+test('create a manager object', () => {
     const manager = new Manager();
 });
+
+test('get the office number of manager', () => {
+    const manager = new Manager();
+
+    // expect(manager.get)
+})
