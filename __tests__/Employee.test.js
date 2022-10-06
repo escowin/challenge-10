b4b63887b50ad6cff2d3ba2ee4de15c5b29838da
escowin <edwin@escowinart.com>
2022-10-05 23:02:12 -0500

    tdd Employee properties & methods pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -8,3 +8,26 @@ test('create an Employee object', () => {
     expect(employee.email).toBe('bateman@dorsia.com')
 });
 
+test('get the Employee name', () => {
+    const employee = new Employee('Paul Allen', 2, 'allen@dorsia.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+});
+
+test('get the Employee ID no.', () => {
+    const employee = new Employee('Jane', 3, 'jane@dosia.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
+
+test('get the Employee email address', () => {
+    const employee = new Employee('Craig McDermott', 4, 'mcdermott@dorsia.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+});
+
+test('get the role of the Employee', () => {
+    const employee = new Employee('')
+
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
+});
