9528c4f2275dfd429eface8409f8598ab1713199
escowin <edwin@escowinart.com>
2022-10-09 19:01:22 -0500

    initial mobile styles

--- src/page-template.js
+++ src/page-template.js
@@ -88,6 +88,7 @@ const generatePage = team => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel='stylesheet' href='styles.css'>
         <title>Team profile</title>
     </head>
 
